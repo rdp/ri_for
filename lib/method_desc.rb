a3602aac016728b7310206dd37a5d3636a454e5e
rogerdpack <rogerpack2005@gmail.com>
2009-07-16 16:23:32 -0600

    put our output at the bottom

--- lib/method_desc.rb
+++ lib/method_desc.rb
@@ -7,11 +7,33 @@ module SourceLocationDesc
   # add a Method#desc which reads off the method's rdocs if any exist
   # 1.9 only
   def desc want_output = false
-    doc = [to_s, "arity: #{arity}"]
-    if respond_to? :source_location
-      file, line = source_location
+    doc = []
+
+    # to_s is something like "#<Method: String#strip>"
+    to_s =~ /ethod: ((.*)#(.*))>/
+    full_name = $1
+    class_name = $2
+    method_name = $3
+    
+    
+    # now default RI for the same:
+    begin
+      RDoc::RI::Driver.run [full_name]
+    rescue SystemExit
+     # not found
     end
-    if file
+
+
+    # now gather up any other information we now about it, in case there are no rdocs
+
+    if !(respond_to? :source_location)
+      # pull out names for 1.8
+      gem 'arguments' # TODO why is this necessary?
+      require 'arguments' # rogerdpack-arguments
+      doc << Arguments.names( eval(class_name), method_name)
+    else
+      file, line = source_location
+     if file
       # then it's a pure ruby method
       head_and_sig = File.readlines(file)[0...line]
       sig = head_and_sig[-1]
@@ -20,28 +42,22 @@ module SourceLocationDesc
       # needs more sophistication, but well... :)
       head.reverse_each do |line|
         break unless line =~ /^\s*#(.*)/
-        doc.unshift $1.strip
+        doc.unshift "     " + $1.strip
       end
-    else
-      doc << 'Binary method or you\'re not in 1.9'
+     else
+      doc << 'binary method (c)'
+     end
     end
 
     if respond_to? :parameters
-      prog_sig = "Programmatic signature: %s %p" % [name, parameters]
-      orig_sig = "Original signature: %s" % sig.to_s.strip
-      puts [prog_sig, orig_sig, ''] + doc
-    else
-      puts doc
+      prog_sig = "Signature from #parameters: %s %p" % [name, parameters]
+      orig_sig = "Original code signature: %s" % sig.to_s.strip
+      doc = [prog_sig, orig_sig, ''] + doc
     end
+    # put arity at the end
+    doc += [to_s, "arity: #{arity}"]
+    puts doc
 
-    # now default RI for the same:
-    # to_s is something like "#<Method: String#strip>"
-    to_s =~ /ethod: (.*)>/
-    begin
-      RDoc::RI::Driver.run [$1]
-    rescue SystemExit
-     # not found
-    end
     doc if want_output
   end
 end
@@ -62,26 +78,14 @@ class Object
   end
 end
 
-if $0 == __FILE__
-  require 'pathname'
-  puts "=" * 80
-  puts Pathname.instance_method(:children).desc
-  puts "=" * 80
-  puts Pathname.instance_method(:ftype).desc
-  puts "=" * 80
-  # now some binaries:
-  puts ''.method_desc :strip
-  puts String.method_desc :strip
-end
-
 =begin 
->> require 'pathname'
 doctest:
- it should display the name
->> Pathname.instance_method(:children).desc(true).grep(/children/)
-=>  ["#<UnboundMethod: Pathname#children>"]
+>> require 'pathname'
+it should display the name
+>> Pathname.instance_method(:children).desc(true).grep(/children/).size > 0
+=>  true # ["#<UnboundMethod: Pathname#children>"]
 
- and arity
+and arity
 >> Pathname.instance_method(:children).desc(true).grep(/arity/)
 =>  ["arity: -1"]
 
